25d2cb2ce4314fb39504fd21149fd959fc654689
ibrahim öz <ibrahimoz.mail@gmail.com>
2023-04-27 01:29:33 +0200

    Express and ejs modules are created.

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 //jshint esversion:6
 
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-app.length("/", function(req, res){
-    res.send("Hello");
+
+app.set('view engine', 'ejs');
+
+app.get("/", function (req, res) {
+
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    if (currentDay === 6 || currentDay === 0) {
+        day = "Weekend: " + days[currentDay]
+    } else {
+        day = "Weekday: " + days[currentDay]
+    }
+
+    res.render("list", {kindOfDay: day})
+    
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server is running.")
 });
